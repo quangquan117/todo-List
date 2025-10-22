52e5b102f8c949319a220a4adf5c6fde4023644f
quangquan <leo.langlois2@gmail.com>
2025-10-22 15:01:17 +0200

    [ADD] Card.jsx (print Task)

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card( { titre } ) {
+    return (
+        <div className="card">
+            <h3>{titre}</h3>
+            {/* Ajouter le bouton done et supprimer */}
+        </div>
+    )
+}
